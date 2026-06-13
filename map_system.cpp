996a900f938be3946205fb035cf0686842456bc9
EndlessHole-gg <alexandra.va.durova@gmail.com>
2026-06-13 10:52:06 +0300

    change

--- map_system.cpp
+++ map_system.cpp
@@ -125,4 +125,211 @@ void Level::generateContent() {
     exit_point = kPointNorthEdge;
 
     getPlane(kPlaneFloor)->getPoint(kPointWestEdge)->setResource(kResourceNote);
-        getPlane(kPlaneFloor
+    getPlane(kPlaneFloor)->getPoint(kPointWestEdge)->setNoteChar('R');
+    getPlane(kPlaneFloor)
+        ->getPoint(kPointEastEdge)
+        ->setResource(kResourceCoffee);
+    return;
+  }
+
+  if (floor_number == 4) {
+    getPlane(kPlaneFloor)->getPoint(kPointSouthEdge)->setMerchant(true);
+  }
+
+  // генерация ловушек, врагов и случайных ресурсов
+  for (int i = 0; i < 6; ++i) {
+    Plane* current_plane = getPlane(i);
+    if (!current_plane) continue;
+
+    for (int j = 0; j < 5; ++j) {
+      if (i == kPlaneFloor && j == kPointCenter) continue;
+      if (i == kPlaneFloor && j == kPointNorthEdge) continue;
+      if (floor_number == 4 && i == kPlaneFloor && j == kPointSouthEdge)
+        continue;
+
+      int r = rand() % 100;
+
+      // ловушки в зависимости от этажа
+      if (r < 10) {
+        if (floor_number == 1)
+          current_plane->getPoint(j)->setTrap(kTrapCobweb);
+        else if (floor_number == 2)
+          current_plane->getPoint(j)->setTrap(kTrapStickyTape);
+        else if (floor_number == 3)
+          current_plane->getPoint(j)->setTrap(kTrapDichlorvos);
+      } else if (r < 18 && floor_number >= 2) {
+        current_plane->getPoint(j)->setTrap(kObstacleBoxes);
+      } else if (r < 33 && !(floor_number == 3 && i == kPlaneFloor)) {
+        current_plane->getPoint(j)->setHasEnemy(true);
+      } else {
+        if (rand() % 100 < kResourceSpawnChance) {
+          int type = rand() % 100;
+          if (type < kRoachAllyChance) {
+            current_plane->getPoint(j)->setResource(kResourceRoach);
+          } else if (type < kRoachAllyChance + kCollectableChance) {
+            // секретки спавнятся отдельно
+          } else if (type < kRoachAllyChance + kCollectableChance +
+                                kSpellScrollChance) {
+            current_plane->getPoint(j)->setResource(kResourceSpellScroll);
+            const char* spells[] = {"U", "JKL", "GHJ"};
+            current_plane->getPoint(j)->setSpellSequence(spells[rand() % 3]);
+          } else {
+            current_plane->getPoint(j)->setResource(kResourceCoffee);
+          }
+        }
+      }
+    }
+  }
+
+  // секретки (гарантированно 1 на уровень)
+  if (floor_number >= 1 && floor_number <= 3) {
+    bool placed = false;
+    for (int attempt = 0; attempt < 100 && !placed; ++attempt) {
+      int plane_idx = rand() % 6;
+      int point_idx = rand() % 5;
+      if (plane_idx == kPlaneFloor &&
+          (point_idx == kPointCenter || point_idx == kPointNorthEdge))
+        continue;
+      Point* pt = getPlane(plane_idx)->getPoint(point_idx);
+      if (pt->getTrap() == "None" && !pt->getHasEnemy() && !pt->hasResource() &&
+          !pt->isVent() && !pt->hasMerchant()) {
+        pt->setResource(kResourceCollectable);
+        has_collectable_on_level = true;
+        placed = true;
+      }
+    }
+  }
+
+  // вентиляция
+  for (int i = 0; i < 6; ++i) {
+    if (i == kPlaneFloor) continue;
+    Plane* pl = getPlane(i);
+    if (!pl) continue;
+    for (int j = 0; j < 5; ++j) {
+      Point* pt = pl->getPoint(j);
+      if (pt->getTrap() != "None" || pt->getHasEnemy() || pt->hasResource())
+        continue;
+      if (rand() % 100 < kVentSpawnChance) pt->setVent(true);
+    }
+  }
+
+  // спавн торговца
+  if (floor_number >= 1 && floor_number <= 3) {
+    while (true) {
+      int m_plane = rand() % 6;
+      int m_point = rand() % 5;
+      if (m_plane == kPlaneFloor &&
+          (m_point == kPointCenter || m_point == kPointNorthEdge))
+        continue;
+      Point* pt = getPlane(m_plane)->getPoint(m_point);
+      if (pt->getTrap() == "None" && !pt->getHasEnemy() && !pt->hasResource() &&
+          !pt->isVent()) {
+        pt->setMerchant(true);
+        break;
+      }
+    }
+  }
+
+  // босс и выход
+  if (floor_number >= 1 && floor_number <= 3) {
+    std::vector<int> free_centers;
+    std::vector<std::pair<int, int>> free_spots;
+
+    for (int pl = 0; pl < 6; ++pl) {
+      for (int pt = 0; pt < 5; ++pt) {
+        Point* p = getPlane(pl)->getPoint(pt);
+        if (p->getTrap() == "None" && !p->getHasEnemy() && !p->hasResource() &&
+            !p->isVent() && !p->hasMerchant()) {
+          free_spots.push_back({pl, pt});
+          if (pt == kPointCenter) {
+            free_centers.push_back(pl);
+          }
+        }
+      }
+    }
+
+    if (!free_centers.empty()) {
+      boss_plane = free_centers[rand() % free_centers.size()];
+      boss_point = kPointCenter;
+      for (auto it = free_spots.begin(); it != free_spots.end(); ++it) {
+        if (it->first == boss_plane && it->second == boss_point) {
+          free_spots.erase(it);
+          break;
+        }
+      }
+    } else {
+      boss_plane = kPlaneFloor;
+      boss_point = kPointCenter;
+    }
+
+    if (!free_spots.empty()) {
+      int idx = rand() % free_spots.size();
+      exit_plane = free_spots[idx].first;
+      exit_point = free_spots[idx].second;
+    } else {
+      exit_plane = kPlaneFloor;
+      exit_point = kPointNorthEdge;
+    }
+  } else if (floor_number == 4) {
+    boss_plane = kPlaneFloor;
+    boss_point = kPointCenter;
+    exit_plane = kPlaneFloor;
+    exit_point = kPointNorthEdge;
+  }
+
+  // ноты на уровне
+  if (floor_number >= 1 && floor_number <= 3) {
+    std::vector<char> floor_notes;
+    if (floor_number == 1)
+      floor_notes = {'T', 'Y', 'U'};
+    else if (floor_number == 2)
+      floor_notes = {'G', 'H'};
+    else if (floor_number == 3)
+      floor_notes = {'J', 'K'};
+
+    for (char note_char : floor_notes) {
+      std::vector<std::pair<int, int>> free_spots_for_notes;
+      for (int pl = 0; pl < 6; ++pl) {
+        for (int pt = 0; pt < 5; ++pt) {
+          Point* p = getPlane(pl)->getPoint(pt);
+          if (p->getTrap() == "None" && !p->getHasEnemy() &&
+              !p->hasResource() && !p->isVent() && !p->hasMerchant()) {
+            bool is_boss = (pl == boss_plane && pt == boss_point);
+            bool is_exit = (pl == exit_plane && pt == exit_point);
+            if (!is_boss && !is_exit) {
+              free_spots_for_notes.push_back({pl, pt});
+            }
+          }
+        }
+      }
+      if (!free_spots_for_notes.empty()) {
+        int idx = rand() % free_spots_for_notes.size();
+        Point* p = getPlane(free_spots_for_notes[idx].first)
+                       ->getPoint(free_spots_for_notes[idx].second);
+        p->setResource(kResourceNote);
+        p->setNoteChar(note_char);
+      }
+    }
+  }
+
+  // добиваем кофе на свободные места
+  if (floor_number > 0) {
+    for (int i = 0; i < 6; ++i) {
+      Plane* pl = getPlane(i);
+      if (!pl) continue;
+      for (int j = 0; j < 5; ++j) {
+        Point* pt = pl->getPoint(j);
+        if (pt->getTrap() == "None" && !pt->getHasEnemy() &&
+            !pt->hasResource() && !pt->isVent() && !pt->hasMerchant()) {
+          bool is_boss = (i == boss_plane && j == boss_point);
+          bool is_exit = (i == exit_plane && j == exit_point);
+          if (!is_boss && !is_exit) {
+            if (rand() % 100 < 20) {
+              pt->setResource(kResourceCoffee);
+            }
+          }
+        }
+      }
+    }
+  }
+}
